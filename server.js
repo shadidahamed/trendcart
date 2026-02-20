7690a49e86ad5d7face32a12d6dc9361bb8dd72b
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-02-20 23:41:03 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
