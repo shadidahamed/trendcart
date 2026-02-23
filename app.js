99c7151d222eea5076ee1333b53d039f535d3a4a
Shadid Ahamed <shadidahamed.matashome05m@gmail.com>
2026-02-23 20:23:42 +0600

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,20 @@ const products = [
     category: "books",
     sub: "manga",
     title: "Popular Manga Volume 1",
-    price: 15.99,
-    image: "(PUT IMAGE URL HERE)",
-    link: "(PUT YOUR AMAZON AFFILIATE LINK HERE)"
+    price: 99.97 ,
+    image: "https://m.media-amazon.com/images/I/81+R+a2MIQL._SL1500_.jpg",
+    link: "https://www.amazon.com/Gachiakuta-9-Book-Complete-Collection-Book/dp/B0DKVXCXF5?crid=25GS112ES3HCB&dib=eyJ2IjoiMSJ9.b3o8tZfIp2vhPDHLgChtT9B4C7JX_JAd41oIFzrBeasWzU8GRuox_wd5nW5Z-XTP7yld_jl-DlvGb8msF9x2TcbW1efItJkWRH2VVodP8SVMmyFe6OVP4NsqHNDC5Lb9CBP6n3DP5ewHtpcES8rwtVJN9XRFWRPnRVSLAFusHN4oALqVi19rlDv2cW8ro9Z2Kuq0lxhD3WS5MPzzuyvnGxO5hanx51cPGMyEF5e7UQU.pB6nyypYSAf5XXpmjz83yj8YVcOYK3RZqjxLxxYXa0g&dib_tag=se&keywords=manga+gachiakuta+set&qid=1771856327&sprefix=manga+gachi%2Caps%2C355&sr=8-1&linkCode=ll2&tag=trendcartbd-20&linkId=1da5fe09ad65ed694e2217a8da0b71b3&language=en_US&ref_=as_li_ss_tl"
+  }
+,
+   {
+    category: "books",
+    sub: "manga",
+    title: "Popular Manga Volume 2",
+    price: 245.00 ,
+    image: "https://m.media-amazon.com/images/I/813xQsPvyRL._SL1500_.jpg",
+    link: "https://www.amazon.com/Leveling-Manga-Collection-Books-Chugong/dp/1637995164?crid=1FTETXUULDNBI&dib=eyJ2IjoiMSJ9.X0BOOuAzK7WeORDt3jih945NuiUC_TW3OBZLvSECoDMoFerZ3Vq9dOCIO8_zRF9NiRKQCO8Jdx-rxPw9qYb2mzTx6WlzQnqf-yJPWHrVWx6isb3BSjAEyX0KBoDeX8Wtq3JC0XOAydC0QOT7VaHVs17Wz5CRnMG377IAAeGe7n-MOawRujdQ0mhNBc9C5gkv_AvVuY9lu_wuKT-oQOBi1Cs960JlY-pKcyoIE6jeXH4.LiSIWWCop1HEO2IwzFowBX-zWE0eGESUT_mvIMPrZxw&dib_tag=se&keywords=manga+solo+leveling+box+set&qid=1771856535&sprefix=manga+solo%2Caps%2C350&sr=8-1&linkCode=ll2&tag=trendcartbd-20&linkId=2ea5a632e15634d1e197cfc62b78228e&language=en_US&ref_=as_li_ss_tl"
   },
+
   {
     category: "clothing",
     sub: "men",
